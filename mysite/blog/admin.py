52bc0be26bb49a1ec69875a32705d6e8588ce43d
Matirivarola89 <matiasgonzalorivarola@gmail.com>
2025-08-16 21:40:55 -0300

    Delete mysite directory

--- mysite/blog/admin.py
+++
@@ -1,8 +0,0 @@
-# blog/admin.py
-
-from django.contrib import admin
-from .models import Post, Categoria, Comentario
-
-admin.site.register(Post)
-admin.site.register(Categoria)
-admin.site.register(Comentario)
